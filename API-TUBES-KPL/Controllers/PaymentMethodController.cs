a8ee0c2a1af4dba2891d78925ba33df9f0a21d15
unknown <haikalalfaro123@gmail.com>
2024-05-12 23:32:15 +0700

    API

---
+++ API-TUBES-KPL/Controllers/PaymentMethodController.cs
@@ -0,0 +1,25 @@
+﻿using API_TUBES_KPL.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API_TUBES_KPL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentMethodController : Controller
+    {
+        private static List<PaymentMethod> DataPayment = new List<PaymentMethod>();
+        public PaymentMethodController() {
+            string jsonFilePath = "C:\\Users\\Haikal\\OneDrive\\Documents\\Konstruksi Perangkat Lunak\\API-TUBES-KPL\\API-TUBES-KPL\\Data\\DataPaymentMethod.JSON";
+            string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+            DataPayment = JsonConvert.DeserializeObject<List<PaymentMethod>>(jsonData);
+        }
+
+        [HttpGet]
+        public IEnumerable<PaymentMethod> Get()
+        {
+            return DataPayment;
+        }
+
+    }
+}
---
+++ API-TUBES-KPL/Model/PaymentMethod.cs
@@ -0,0 +1,20 @@
+﻿namespace API_TUBES_KPL.Model
+{
+    public class PaymentMethod
+    {
+        public string ID_Payment { get; set; }
+        public string Nama_Pembeli { get; set; }
+        public string Metode_Pembayaran { get; set; }
+        public int Jumlah { get; set; }
+        public int Biaya_Admin { get; set; }
+
+        public PaymentMethod(string ID_Payment, string Nama_Pembeli, string Metode_Pembayaran, int Jumlah, int Biaya_Admin)
+        {
+            this.ID_Payment = ID_Payment;
+            this.Nama_Pembeli = Nama_Pembeli;
+            this.Metode_Pembayaran = Metode_Pembayaran;
+            this.Jumlah = Jumlah;
+            this.Biaya_Admin = Biaya_Admin;
+        }
+    }
+}
