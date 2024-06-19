e0633fd9b9475d8ee30dd8203e4c4477b3a87c4d
NaufalAjhar <74417421+NaufalAjhar@users.noreply.github.com>
2024-06-19 14:21:16 +0700

    adding payment main

---
+++ Payment-Main/Controller/PaymentController.cs
@@ -0,0 +1,61 @@
+﻿using Payment_API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Payment_Main.Controller
+{
+    public class PaymentController
+    {
+        private List<Payment> payments;
+
+        public PaymentController()
+        {
+            payments = ReadJsonFile("C:\\Users\\Haikal\\OneDrive\\Documents\\Konstruksi Perangkat Lunak\\Payment\\Payment-API\\Data\\dataPayment.json");
+        }
+
+        private List<Payment> ReadJsonFile(string filePath)
+        {
+            List<Payment> daftarHistoryPayment = new List<Payment>();
+            try
+            {
+                string json;
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    json = reader.ReadToEnd();
+                }
+
+                daftarHistoryPayment = JsonSerializer.Deserialize<List<Payment>>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading JSON file: " + ex.Message);
+            }
+            return daftarHistoryPayment;
+        }
+
+        public List<Payment> GetAllPayments()
+        {
+            return payments;
+        }
+
+        public List<Payment> GetPaymentByIDUser(int ID_User)
+        {
+            List<Payment> historyByIDUser = new List<Payment>();
+            for (int i = 0; i < payments.Count; i++)
+            {
+                if (payments[i].ID_User.Equals(ID_User))
+                {
+                    historyByIDUser.Add(payments[i]);
+                }
+            }
+            return historyByIDUser;
+        }
+
+
+    }
+}
+
---
+++ Payment-Main/Model/Payment.cs
@@ -0,0 +1,23 @@
+﻿namespace Payment_API.Model
+{
+    public class Payment
+    {
+        public int ID_Payment { get; set; }
+
+        public int ID_User { get; set; }
+        public string Nama_Pembeli { get; set; }
+        public string Metode_Pembayaran { get; set; }
+        public int Jumlah { get; set; }
+        public int Biaya_Admin { get; set; }
+
+        public Payment(int ID_Payment, int ID_User, string Nama_Pembeli, string Metode_Pembayaran, int Jumlah, int Biaya_Admin)
+        {
+            this.ID_Payment = ID_Payment;
+            this.ID_User = ID_User;
+            this.Nama_Pembeli = Nama_Pembeli;
+            this.Metode_Pembayaran = Metode_Pembayaran;
+            this.Jumlah = Jumlah;
+            this.Biaya_Admin = Biaya_Admin;
+        }
+    }
+}
